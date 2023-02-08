71ee8468fb5e7163c06b1db552898dae5936e114
Irina Alyabeva <irinpingviiin@yandex.ru>
2023-02-09 01:25:49 +0300

    Практическая работа 15
    Исправления по замечаниям ревьювера

--- backend/app.js
+++ backend/app.js
@@ -5,26 +5,27 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const router = require('./routes/routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const errorHandler = require('./middlewares/errorHandler');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, NODE_ENV, MONGOOSE_URL } = process.env;
 const app = express();
 const allowedCors = [
   'https://mesto.irinpingviiin.nomoredomainsclub.ru',
   'http://mesto.irinpingviiin.nomoredomainsclub.ru',
-  'http://localhost:3000'
+  'http://localhost:3000',
 ];
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect(NODE_ENV === 'production' ? MONGOOSE_URL : 'mongodb://localhost:27017/mestodb');
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
@@ -38,6 +39,7 @@ app.use(function(req, res, next) {
   }
 
   next();
+  return null;
 });
 
 app.use(requestLogger);
@@ -48,14 +50,6 @@ app.use(errorLogger);
 
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-
-  res.status(statusCode).send({
-    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
-  });
-
-  next();
-});
+app.use(errorHandler);
 
 app.listen(PORT);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,7 +10,7 @@ router.get('/users/me', getCurrentUser);
 
 router.get('/users/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24),
+    userId: Joi.string().hex().length(24),
   }),
 }), getUserById);
 
